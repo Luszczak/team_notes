0c22065257ee05a14fef196a91657a04518cdd40
Hugo <hugo145olguin@gmail.com>
2025-08-28 17:16:31 -0300

    feat prueba 2: agregar funcion add_note t comando CLI add

--- app/cli.py
+++ app/cli.py
@@ -1,7 +1,8 @@
 import sys
 from app.notes import add_note
 
-if sys.argv[1] == "add":
-    title = sys.argv[2]
-    body = sys.argv[3]
-    add_note(title, body)
+if __name__ == "__main__":
+    if sys.argv[1] == "add":
+        title = sys.argv[2]
+        body = sys.argv[3]
+        add_note(title, body)
